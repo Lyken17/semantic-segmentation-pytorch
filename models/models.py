d0d62560f5622ef0f1e2c38cedcb14e80e89f0ad
Lyken17 <lykensyu@gmail.com>
2018-09-12 21:00:30 -0400

    remove wrong upsample.

--- models/models.py
+++ models/models.py
@@ -154,12 +154,15 @@ class MobilveNetv2(nn.Module):
 		self.blocks = ori_mobilenet.blocks
 		self.feature_mix_layer = ori_mobilenet.feature_mix_layer
 		# TODO: is here any better any to replace this?
+		self.fix_channels = nn.Conv2d(1280, 2048, 1)
 
 	def forward(self, x, return_feature_maps=False):
+
 		o = self.first_conv(x)
 		for block in self.blocks:
 			o = block(o)
 		o = self.feature_mix_layer(o)
+		o = self.fix_channels(o)
 
 		return [o]
 
@@ -395,8 +398,9 @@ class PPMBilinearDeepsup(nn.Module):
 		input_size = conv5.size()
 		ppm_out = [conv5]
 		for pool_scale in self.ppm:
-			ppm_out.append(nn.functional.upsample(pool_scale(conv5), (input_size[2], input_size[3]), mode='bilinear',
-				align_corners=False))
+			ppm_out.append(
+				nn.functional.upsample(pool_scale(conv5), (input_size[2], input_size[3]), mode='bilinear', align_corners=False)
+			)
 		ppm_out = torch.cat(ppm_out, 1)
 
 		x = self.conv_last(ppm_out)
@@ -432,8 +436,11 @@ class UPerNet(nn.Module):
 		for scale in pool_scales:
 			self.ppm_pooling.append(nn.AdaptiveAvgPool2d(scale))
 			self.ppm_conv.append(
-				nn.Sequential(nn.Conv2d(fc_dim, 512, kernel_size=1, bias=False), SynchronizedBatchNorm2d(512),
-					nn.ReLU(inplace=True)))
+				nn.Sequential(
+					nn.Conv2d(fc_dim, 512, kernel_size=1, bias=False),
+					SynchronizedBatchNorm2d(512),
+					nn.ReLU(inplace=True))
+			)
 		self.ppm_pooling = nn.ModuleList(self.ppm_pooling)
 		self.ppm_conv = nn.ModuleList(self.ppm_conv)
 		self.ppm_last_conv = conv3x3_bn_relu(fc_dim + len(pool_scales) * 512, fpn_dim, 1)
@@ -441,17 +448,24 @@ class UPerNet(nn.Module):
 		# FPN Module
 		self.fpn_in = []
 		for fpn_inplane in fpn_inplanes[:-1]:  # skip the top layer
-			self.fpn_in.append(nn.Sequential(nn.Conv2d(fpn_inplane, fpn_dim, kernel_size=1, bias=False),
-				SynchronizedBatchNorm2d(fpn_dim), nn.ReLU(inplace=True)))
+			self.fpn_in.append(
+				nn.Sequential(nn.Conv2d(fpn_inplane, fpn_dim, kernel_size=1, bias=False),
+				SynchronizedBatchNorm2d(fpn_dim),
+              nn.ReLU(inplace=True))
+			)
 		self.fpn_in = nn.ModuleList(self.fpn_in)
 
 		self.fpn_out = []
 		for i in range(len(fpn_inplanes) - 1):  # skip the top layer
-			self.fpn_out.append(nn.Sequential(conv3x3_bn_relu(fpn_dim, fpn_dim, 1), ))
+			self.fpn_out.append(
+				nn.Sequential(conv3x3_bn_relu(fpn_dim, fpn_dim, 1), )
+			)
 		self.fpn_out = nn.ModuleList(self.fpn_out)
 
-		self.conv_last = nn.Sequential(conv3x3_bn_relu(len(fpn_inplanes) * fpn_dim, fpn_dim, 1),
-			nn.Conv2d(fpn_dim, num_class, kernel_size=1))
+		self.conv_last = nn.Sequential(
+			conv3x3_bn_relu(len(fpn_inplanes) * fpn_dim, fpn_dim, 1),
+			nn.Conv2d(fpn_dim, num_class, kernel_size=1)
+		)
 
 	def forward(self, conv_out, segSize=None):
 		conv5 = conv_out[-1]
@@ -483,6 +497,7 @@ class UPerNet(nn.Module):
 			fusion_list.append(
 				nn.functional.upsample(fpn_feature_list[i], output_size, mode='bilinear', align_corners=False))
 		fusion_out = torch.cat(fusion_list, 1)
+		print(fusion_out.size())
 		x = self.conv_last(fusion_out)
 
 		if self.use_softmax:  # is True during inference
