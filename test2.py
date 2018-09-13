a87ac20fceea731707f80079b55d39ee71b6bcaf
Lyken17 <lykensyu@gmail.com>
2018-09-12 21:04:14 -0400

    remove wrong upsample.

--- test2.py
+++ test2.py
@@ -1,22 +1,26 @@
 import json
-from models.MobileNets import MobileNetV2
 
 import torch
 
-from models.utils import load_url, download_url
+from models.MobileNets import mobilenet_v2
+from models.resnet import resnet50
 
-model_urls = {
-    'mobilenetv2': {
-        "weight": 'http://cloud.syu.life/MobilveNetV2-09-11/model.pth.tar',
-	    "config": "http://cloud.syu.life/MobilveNetV2-09-11/net.config"
-    }
-}
 
-res = download_url(model_urls["mobilenetv2"]["config"])
-with open(res, "r") as fp:
-	cfg = json.load(fp)
-	net = MobileNetV2.build_from_config(cfg)
-	# net.load_state_dict(load_url(model_urls["mobilenetv2"]["weight"]), strict=False)
-	print(net)
-	data = torch.zeros(1, 3, 224, 224)
-	output = net(data)
+from models.models import Resnet, ResnetDilated
+from models.models import MobilveNetv2 as mnet
+
+
+n1 = resnet50()
+n11 = Resnet(n1)
+
+n2 = mobilenet_v2()
+n22 = mnet(n2)
+
+data = torch.zeros(1, 3, 600, 454)
+
+o1 = n11(data)
+o2 = n22(data)
+
+[print(_.size()) for _ in o1]
+print("==============================================================")
+[print(_.size()) for _ in o2]
