106f96782591683ba26beef8145b0ff40d4c00c8
Lyken17 <lykensyu@gmail.com>
2018-09-17 23:34:51 -0400

    remove wrong upsample.

--- test2.py
+++ test2.py
@@ -2,25 +2,50 @@ import json
 
 import torch
 
-from models.MobileNets import mobilenet_v2
+from models.MobileNets import mobilenet_v2, MobilveNetv2_wrapper as mnet
 from models.resnet import resnet50
 
 
 from models.models import Resnet, ResnetDilated
-from models.models import MobilveNetv2 as mnet
-
-
-n1 = resnet50()
-n11 = Resnet(n1)
+#
+# n1 = resnet50()
+# n11 = Resnet(n1)
 
 n2 = mobilenet_v2()
-n22 = mnet(n2)
+device = "cpu"
+if torch.cuda.is_available():
+	device = "cuda"
+n2.to(device)
+# n22 = mnet(n2)
+#
+# data = torch.zeros(1, 3, 600, 454)
+#
+# o1 = n11(data)
+# o2 = n22(data)
+#
+# [print(_.size()) for _ in o1]
+# print("==============================================================")
+# [print(_.size()) for _ in o2]
+#
+#
+# print(n2)
 
-data = torch.zeros(1, 3, 600, 454)
+# dummy_input = torch.zeros(1, 3, 224, 224)
+# torch.onnx.export(n2, dummy_input,"export.onnx", verbose=True, )
 
-o1 = n11(data)
-o2 = n22(data)
+dummy = torch.randn(1, 3, 224, 224).to(device)
 
-[print(_.size()) for _ in o1]
-print("==============================================================")
-[print(_.size()) for _ in o2]
+# warm up
+import time
+start = time.time()
+for i in range(10):
+	out = n2(dummy)
+end = time.time()
+print("duration %.4f " % (end - start))
+
+
+start = time.time()
+for i in range(10):
+	out = n2(dummy)
+end = time.time()
+print("duration %.4f" %  (end - start))
